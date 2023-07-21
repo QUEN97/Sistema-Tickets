2d99d939ff1a3b7212b6d1a2b6806dd077fbe079
QUEN97 <Antonio1308_97@outlook.com>
2023-07-21 12:26:36 -0600

    Busqueda y Filtrado - Areas

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -3,22 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\Areas;
+use App\Models\Departamento;
 use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
+    public $filterSoli;
+    public $deptos;
+
     public function home(Request $request){
-        $areas = Areas::where([
-            ['name', '!=', Null],
-            [function ($query) use ($request) {
-                if (($s = $request->s)) {
-                    $query->orWhere('name', 'LIKE', '%' . $s . '%')
-                        ->get();
+    //     $areas = Areas::where([
+    //         ['name', '!=', Null],
+    //         [function ($query) use ($request) {
+    //             if (($s = $request->s)) {
+    //                 $query->orWhere('name', 'LIKE', '%' . $s . '%')
+    //                     ->get();
+    //             }
+    //         }]
+    //     ])->paginate(10) ->withQueryString();
+    $this->filterSoli = $request->input('filterSoli') == 'Todos' ? null : $request->input('filterSoli');
+
+        $deptos = Departamento::where('status', 'Activo')->get();
+        $depto=Departamento::where('name', 'LIKE', '%' . $request->search . '%')->get();
+
+        $areas = Areas::where(function ($query) use ($request, $depto) {
+                $search = $request->input('search');
+                if ($search && $depto->count() === 0) {
+                    $query->where('id', 'LIKE', '%' . $search . '%')
+                        ->orWhere('name', 'LIKE', '%' . $search . '%')
+                        ->orWhere('status', 'LIKE', '%' . $search . '%');
+                } else {
+                    $query->whereIn('departamento_id', Departamento::where('name', 'LIKE', '%' . $search . '%')->pluck('id'));
                 }
-            }]
-        ])->paginate(10) ->withQueryString();
+            })
+            ->when($request->has('filter') && $request->input('filter') != '', function ($query) use ($request){
+                $filterSoli = $request->input('filter');
+                $query->where('departamento_id', $filterSoli);
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10)
+            ->withQueryString();
         $trashed = Areas::onlyTrashed()->count();
-        return view('modules.areas.areas',compact('areas','trashed'));
+        return view('modules.areas.areas',compact('areas','trashed','deptos'));
     }
 
 
